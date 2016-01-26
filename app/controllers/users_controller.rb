1afe3e04be79992097c55e52dd97861b481bdbb7
Shafaet <shafaet.csedu@gmail.com>
2016-01-26 20:15:15 +0600

    adding userlogin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+	before_action :set_user, only: [:edit,:update,:show]
 	def index
 		@users = User.paginate(page: params[:page], per_page: 5)
 	end
@@ -18,16 +18,13 @@ class UsersController < ApplicationController
 	end
 	
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def show
-		@user = User.find(params[:id])
 		@user_articles = @user.articles.paginate(page: params[:page],per_page: 5)
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:success]="Your account was updated successfully"
 			redirect_to articles_path
@@ -39,5 +36,8 @@ class UsersController < ApplicationController
 	private
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+	def set_user
+		@user = User.find(params[:id])
 	end
 end
