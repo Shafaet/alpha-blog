8967e8795fcefda786278433c06e9009b63a319b
Shafaet <shafaet.csedu@gmail.com>
2016-01-31 18:28:12 +0600

    complete authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
   	if !logged_in?
-  		flash[:danger]="You must be logged in to perfor this action"
+  		flash[:danger]="You must be logged in to perform this action"
   		redirect_to root_path
   	end
   end
