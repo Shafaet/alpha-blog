06654025ac89140c5c794ecfc23522536694cb26
Shafaet <shafaet.csedu@gmail.com>
2016-02-05 02:38:04 +0600

    add admin options

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227101526) do
+ActiveRecord::Schema.define(version: 20160204202931) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151227101526) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
